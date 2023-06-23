1defdc7fa0dd69a786869e1bc561157cfa055e90
Jahel <jahelcuadradomanchado@hotmail.com>
2023-06-23 17:56:15 +0200

    Creado el caché del detalle de la pelicula

--- lib/presentation/providers/movies/movie_detail_provider.dart
+++ lib/presentation/providers/movies/movie_detail_provider.dart
@@ -3,6 +3,7 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//Vamos a guardar los datos de un objeto pelicula en este caso en caché, para ello creamos un provider que va a manejar un mapa de tipos String (que sera el id de la pelicula), Movie (que será el objeto de la pelicula)
 
 
 final movieInfoProvider = StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref) {
@@ -20,6 +21,7 @@ class MovieMapNotifier extends StateNotifier<Map<String, Movie>>{
     required this.getMovie
     }): super({});
 
+  //Ahora, a la hora de pedir la peliculas comprobamos que no exista ya dentro del estado
   Future<void> loadMovie(String movieId) async{
     if ( state[movieId] != null ) return;
     final movie = await getMovie(movieId);
